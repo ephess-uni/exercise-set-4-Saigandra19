7f609c6b01b8da9119c4ea1374c27b0962e51e84
Saigandra19 <164116244+Saigandra19@users.noreply.github.com>
2024-04-27 23:31:49 -0500

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -15,8 +15,3 @@ def logstamp_to_datetime(datestr):
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
-
-# >>>> The code below will call your function and print the results
-if __name__ == "__main__":
-    test_date = '2022-12-01T01:02:03'
-    print(f'{logstamp_to_datetime(test_date)=}')
