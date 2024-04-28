12b08fc1bfe608ddbc6b40a4220ec33b2938ab90
Saigandra19 <164116244+Saigandra19@users.noreply.github.com>
2024-04-27 23:28:37 -0500

    Update ex_4_0.py

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -21,26 +21,3 @@ def get_shutdown_events(logfile):
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f"{get_shutdown_events(FILENAME)=}")
-""" ex_4_0.py """
-try:
-    from src.util import get_data_file_path
-except ImportError:
-    from util import get_data_file_path
-
-# Use this FILENAME variable to test your function.
-FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
-
-
-def get_shutdown_events(logfile):
-    shutdown_lines = []
-    with open(logfile, "r") as f:
-        for line in f:
-            if "Shutdown initiated" in line:
-                shutdown_lines.append(line.strip())
-    return shutdown_lines
-
-
-# >>>> The code below will call your function and print the results
-if __name__ == "__main__":
-    print(f"{get_shutdown_events(FILENAME)=}")
