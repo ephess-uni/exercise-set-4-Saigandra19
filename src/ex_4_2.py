db75e6b38f388e854cb87aeb421e8287a8e8e582
Saigandra19 <164116244+Saigandra19@users.noreply.github.com>
2024-04-27 23:30:21 -0500

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -3,20 +3,6 @@ from datetime import datetime
 
 
 def logstamp_to_datetime(datestr):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
-
-
-# >>>> The code below will call your function and print the results
-if __name__ == "__main__":
-    test_date = '2022-12-01T01:02:03'
-    print(f'{logstamp_to_datetime(test_date)=}')""" ex_4_2.py """
-from datetime import datetime
-
-
-def logstamp_to_datetime(datestr):
     try:
         # Try to parse the date string as-is
         return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
@@ -29,18 +15,6 @@ def logstamp_to_datetime(datestr):
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
-""" ex_4_2.py """
-from datetime import datetime
-
-
-def logstamp_to_datetime(datestr):
-    try:
-        # Try to parse the date string as-is
-        return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
-    except ValueError:
-        # If parsing fails, try to add microseconds and parse again
-        return datetime.strptime(datestr + '.000000', '%Y-%m-%dT%H:%M:%S.%f')
-
 
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
