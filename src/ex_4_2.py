1f2d8a32c42970263329df8c3d64ee4b482971ad
Saigandra19 <164116244+Saigandra19@users.noreply.github.com>
2024-04-27 23:17:51 -0500

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -12,4 +12,20 @@ def logstamp_to_datetime(datestr):
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
+    print(f'{logstamp_to_datetime(test_date)=}')""" ex_4_2.py """
+from datetime import datetime
+
+
+def logstamp_to_datetime(datestr):
+    try:
+        # Try to parse the date string as-is
+        return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        # If parsing fails, try to add microseconds and parse again
+        return datetime.strptime(datestr + '.000000', '%Y-%m-%dT%H:%M:%S.%f')
+
+
+# >>>> The code below will call your function and print the results
+if __name__ == "__main__":
+    test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
